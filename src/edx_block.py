fb5de0b60035270e2078455998898f8cada145fd
Joshua Powers <powersj@gatech.edu>
2015-11-06 20:23:53 -0700

    adding icon sample data to show off icons

--- src/edx_block.py
+++ src/edx_block.py
@@ -24,14 +24,38 @@ class Block(object):
             self.icon = '\uf111'
 
         if self.type == 'problem':
-            self.markdown = block['markdown']
+            try:
+                self.markdown = block['markdown']
+            except KeyError:
+                self.markdown = ''
         if self.type == 'video':
-            self.video_start = block['start_time']
-            self.video_end = block['end_time']
-            self.youtube_id = block['youtube_id']
+            try:
+                self.youtube_id = block['youtube_id']
+            except KeyError:
+                self.youtube_id = ''
+                
+            try:
+                self.video_start = block['start_time']
+            except KeyError:
+                self.video_start = ''
+
+            try:
+                self.video_end = block['end_time']
+            except KeyError:
+                self.video_end = ''
+
+        self.generate_tooltip()
 
     def __str__(self):
         """Overload string function."""
         return '{"name": "%s", "parent": "%s", "type": "%s", "icon": "%s", "tip": "%s"},' % (
                self.name.encode('utf-8'), self.parent.encode('utf-8'), self.type.encode('utf-8'),
                self.icon, self.tip)
+
+    def generate_tooltip(self):
+        if self.type == 'video':
+            self.tip = 'video tip: %s %s %s' % (self.youtube_id.encode('utf-8'),
+                                                self.video_start,
+                                                self.video_end)
+        elif self.type == 'problem' and self.markdown:
+            self.tip = 'I HAVE MARKDOWN TO VIEW!!!'
