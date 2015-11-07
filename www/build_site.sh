c97f0942a6849f54acdaab25947cbfaab75fc933
Joshua Powers <powersj@gatech.edu>
2015-11-06 20:54:23 -0700

    adding checks

--- www/build_site.sh
+++ www/build_site.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-mkdir content
-hugo server --theme=hugo-creative-theme --buildDrafts
+set -eux
+
+# These directories are empty, so git does not check them in
+# therefore, make sure they exist
+mkdir -p content
+mkdir -p layouts
+
+hugo server --theme=hugo-creative-theme --watch --verbose
 
