ab7ed5d91fa5a92a1eb8a4b19ed411fc4cd9f550
Joshua Powers <powersj@gatech.edu>
2015-11-17 19:20:28 -0700

    new deployment script

--- www/site_deploy.sh
+++ www/site_deploy.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-build_site.sh
+./site_deploy.sh
 
 if [ -d "/tmp/public" ];
     then rm -rf /tmp/public
 fi
 
-cp -R public /tmp/public
+cp -R public/ /tmp/public
+
+cd ..
 git checkout gh-pages
+
+# nuclear option, trying to avoid this
+# rm -rf *
+cp -R /tmp/public/* .
+
 git add -A
 git commit -m "Updating github pages"
 git push origin gh-pages
+
+if [ -d "/tmp/public" ];
+    then rm -rf /tmp/public
+fi
+
+git checkout origin
+
+exit
