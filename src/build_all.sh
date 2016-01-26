a6de4eed95d88f0785a7826e772c297a399e25bf
Joshua Powers <mrpowersj@gmail.com>
2016-01-25 20:03:37 -0700

    updating run all, lot simpler

--- src/build_all.sh
+++ src/build_all.sh
@@ -5,9 +5,6 @@
 
 for file in $(find ../input/ -type f -name "*.json")
 do
-    BASENAME=$(echo "${file##*/}" | cut -d'.' -f1)
-    DATE=$(date +%Y-%m-%d)
-
     echo "$BASENAME"
-    ./edx_course_html.py "$file" --id-only > ../output/"$BASENAME"_"$DATE".html
+    ./edx_course_html.py "$file" --id-only
 done
